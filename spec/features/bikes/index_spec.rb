7ae676b58d27f63a72ec8a02477bebbf06275522
Patrick Quigley <patrickquigley102@gmail.com>
2018-06-14 19:35:31 +0100

    Test unfiltered index content.
    First pass at tests for the following stories:
    - I would like to see a list/grid of bikes based on /app/bikes.json
    - I would like to see a the name, an image, a description and the
    class for each bike

--- spec/features/bikes/index_spec.rb
+++ spec/features/bikes/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Bikes application', type: :feature, js: true do
     Capybara.app_host = 'http://127.0.0.1:80'
   end
 
-  describe 'the index page' do
+  describe 'visiting \the index page' do
     before do
       visit '/'
     end
@@ -20,6 +20,52 @@ RSpec.describe 'Bikes application', type: :feature, js: true do
 
       it 'contains a grid of appropriate product count' do
         expect(page).to have_css('div.prod', count: index_item_count)
+      end
+
+      it 'lists all bike names' do
+        bike_data['items'].each do |bike|
+          expect(page).to have_css('div', text: bike['name'])
+        end
+      end
+
+      it 'lists all bike descriptions' do
+        bike_data['items'].each do |bike|
+          expect(page).to have_css('div', text: bike['description'])
+        end
+      end
+
+      it 'lists all bike classes' do
+        bike_data['items'].each do |bike|
+          expect(page).to have_css(
+            'div',
+            text: bike['class'].map(&:capitalize).join(', ')
+          )
+        end
+      end
+
+      context 'large window size' do
+        before do
+          # Using Bootstrap's container width cut off points
+          page.driver.browser.manage.window.resize_to(993,1000)
+        end
+        # Serves thumbnails, although large image is available
+        it 'lists all bike image thumbnails' do
+          bike_data['items'].each do |bike|
+            expect(page).to have_css("img[src*='#{bike['image']['thumb']}']")
+          end
+        end
+      end
+
+      context 'small window size' do
+        before do
+          # Using Bootstrap's container width cut off points
+          page.driver.browser.manage.window.resize_to(575,1000)
+        end
+        it 'lists all bike image thumbnails' do
+          bike_data['items'].each do |bike|
+            expect(page).to have_css("img[src*='#{bike['image']['thumb']}']")
+          end
+        end
       end
     end
   end
